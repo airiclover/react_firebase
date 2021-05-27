5420693cd0cee1eaa1bab5b2d3d409fe6490950f
airi <airi-love-clover@rakuten.jp>
2021-05-27 22:36:27 +0900

    ✨ Add task function

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { db } from "./firebase";
 
 const App = () => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
@@ -14,10 +15,20 @@ const App = () => {
     return () => unSub();
   }, []);
 
+  const newTask = () => {
+    db.collection("tasks").add({ title: input });
+    setInput("");
+  };
+
   return (
     <div className="App">
+      <h1>Todo App by React/firebase</h1>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <button disabled={!input} onClick={newTask}>
+        Click!
+      </button>
       {tasks.map((task) => (
-        <h3>{task.title}</h3>
+        <h3 key={task.id}>{task.title}</h3>
       ))}
     </div>
   );
